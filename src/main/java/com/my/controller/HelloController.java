484c0ae7b0e7af0f34356291e00a2f5a6c729053
YJ <1445156373@qq.com>
2018-06-09 20:47:21 +0800

    first conmmit

---
+++ src/main/java/com/my/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.my.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+
+        return "hello world!";
+    }
+
+    @RequestMapping(value = "/world", method = RequestMethod.GET)
+//    @PostMapping("/world")
+//    @GetMapping("/world")
+    public String world() {
+
+        return " world!";
+    }
+
+    @RequestMapping("/test")
+    public void ok() {
+
+    }
+}
