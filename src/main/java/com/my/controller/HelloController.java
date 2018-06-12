7552c0bcf43b28c6a8d10a8f3ebc4496bae37275
YJ <1445156373@qq.com>
2018-06-12 19:33:17 +0800

    second commit..

--- src/main/java/com/my/controller/HelloController.java
+++ src/main/java/com/my/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/hello")
+//@RequestMapping(value = "/hello")
 public class HelloController {
 
     @RequestMapping("/hello")
